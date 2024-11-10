18bd8616239da22d8d92706a18ba0c98d897d831
Brandon Gerber <90160923+brandongerber@users.noreply.github.com>
2024-11-10 16:32:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,39 @@ import yfinance as yahooFinance
 
 
 def getStockValue(symbol, quantity):
-    stock = yahooFinance.Ticker(symbol)
-    current_price = stock.info['currentPrice']
-    return current_price * quantity
+    try:
+        stock = yahooFinance.Ticker(symbol)
+        current_price = stock.info.get('currentPrice')
+        if current_price is not None:
+            return current_price * quantity
+        else:
+            print(f"Error: Could not retrieve price for {symbol}.")
+            return 0
+    except Exception as e:
+        print(f"Exception occurred for {symbol}: {e}")
+        return 0
 
 
 def getPortfolioValue(stocks):
-    total_value = sum(getStockValue(
-        stock['symbol'], stock['quantity']) for stock in stocks)
+    total_value = sum(getStockValue(stock['symbol'], stock['quantity']) for stock in stocks)
     return total_value
 
-// change the symbols and quantity to whatever you want 
+
 stocks = [
-    {"symbol": "AAPL", "quantity": 3},
-    {"symbol": "GOOG", "quantity": 5},
-    {"symbol": "TSLA", "quantity": 2},
+    {"symbol": "GOGL", "quantity": 3},
+    {"symbol": "MDT", "quantity": 2},
+    {"symbol": "PLTR", "quantity": 5},
+    {"symbol": "ZETA", "quantity": 8},
+    {"symbol": "TTOO", "quantity": 5},
+    {"symbol": "ATCH", "quantity": 14},
 ]
 
 portfolio_value = getPortfolioValue(stocks)
 
-print("Your current Portfolio Status is:")
+print("Portfolio Status:")
 for stock in stocks:
     stock_value = getStockValue(stock['symbol'], stock['quantity'])
     print(
-        f"Amount {stock['quantity']} of Shares {stock['symbol']}, Total value: {stock_value}")
+        f"Holding {stock['quantity']} shares of {stock['symbol']}, Total value: {stock_value}")
 
 print(f"Total portfolio value: {portfolio_value}")
